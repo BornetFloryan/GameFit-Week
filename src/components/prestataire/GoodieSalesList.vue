cf60fe1fc8551ddf26cc8eab67f9aec3c586966d
Adam KHNAGUI <144346140+AdamKHNAGUI@users.noreply.github.com>
2025-01-06 14:38:43 +0100

    GoodieSalesList.vue + affichage propre

--- src/components/prestataire/GoodieSalesList.vue
+++ src/components/prestataire/GoodieSalesList.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="goodie-sales-list">
     <h2>Liste des ventes de goodies</h2>
+
+    <button class="add-goodies">Ajouter un Goodies</button>
     <table>
       <thead>
       <tr>
-        <th>Numéro</th>
-        <th>Goodie</th>
-        <th>Quantité</th>
-        <th>Date</th>
+        <th scope="col">Numéro</th>
+        <th scope="col">Goodie</th>
+        <th scope="col">Quantité</th>
+        <th scope="col">Date</th>
+
       </tr>
       </thead>
       <tbody>
       <tr v-for="sale in sales" :key="sale._id">
         <td>{{ sale._id }}</td>
-        <td>{{ getGoodieName(sale.goodie_id) }}</td>
+        <td>{{ sale.goodie.name }}</td>
         <td>{{ sale.quantity }}</td>
         <td>{{ formatDate(sale.date) }}</td>
       </tr>
@@ -23,26 +26,23 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { sale } from "@/datasource/data.js";
 
 export default {
-  name: 'GoodieSalesList',
-  computed: {
-    ...mapState('goodies', ['sales', 'goodies']),
+  name: "GoodieSalesList",
+  data() {
+    return {
+      sales: sale,
+    };
   },
   methods: {
-    getGoodieName(goodieId) {
-      const goodie = this.goodies.find(g => g._id === goodieId);
-      return goodie ? goodie.name : 'Inconnu';
-    },
     formatDate(date) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      return new Date(date).toLocaleDateString('fr-FR', options);
+      const options = { year: "numeric", month: "long", day: "numeric" };
+      return new Date(date).toLocaleDateString("fr-FR", options);
     },
   },
 };
 </script>
-
 <style scoped>
 .goodie-sales-list {
   max-width: 800px;
@@ -62,5 +62,17 @@ th, td {
 th {
   background-color: #f2f2f2;
   text-align: left;
+}
+
+button.add-goodies {
+  background-color: #007bff;
+  color: white;
+  padding: 10px 20px;
+  margin: 5px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, transform 0.3s ease;
 }
 </style>
