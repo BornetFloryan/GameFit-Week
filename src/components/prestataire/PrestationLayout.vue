fb284a1e9a83af2e22982f6b7c06076cfba5b2af
Adam KHNAGUI <144346140+AdamKHNAGUI@users.noreply.github.com>
2025-01-06 14:41:39 +0100

    Affichage client Goodies resto

---
+++ src/components/prestataire/PrestationLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NavView />
+    <router-view></router-view> <!-- Les vues enfants (goodies, restaurant) seront rendues ici -->
+  </div>
+</template>
+
+<script>
+import NavView from "@/components/NavBar.vue";
+
+export default {
+  name: "PrestationLayout",
+  components: {NavView},
+};
+</script>
+
+<style scoped>
+/* Ajoutez des styles pour le composant */
+</style>
