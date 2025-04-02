360a805d1fa195a980b33b4d5cdb2a0bdbf0a1e4
BRENDLE Corentin <64205741+BrendleCorentin@users.noreply.github.com>
2025-04-02 16:53:33 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import VueI18n from 'vue-i18n'; 
 
-Vue.config.productionTip = false
+
+import en from './i18n/en';
+import fr from './i18n/fr';
+
+Vue.config.productionTip = false;
+
+
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: 'fr',
+  messages: {
+    en,
+    fr
+  }
+});
+
 
 new Vue({
   router,
   store,
+  i18n, // Ajouter i18n à l'instance Vue
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
