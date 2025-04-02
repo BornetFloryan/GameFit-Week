ecf7898877f34f23cde2fcd268124903a75cc751
BRENDLE Corentin <64205741+BrendleCorentin@users.noreply.github.com>
2025-04-02 16:51:40 +0200

    Add files via upload

---
+++ src/i18n/en.js
@@ -0,0 +1,67 @@
+export default {
+    nav: {
+      home: 'Home',
+      ticketing: 'Ticketing',
+      services: 'Services',
+      live: 'Live',
+      login: 'Login',
+      logout: 'Logout',
+      profile: 'Profile',
+      orders: 'Orders',
+      reservations: 'Reservations',
+      tickets: 'Tickets',
+      dashboard: 'Dashboard',
+      request: 'Request',
+      providerList: 'Provider List',
+      dedicaces: 'Dedications',
+      tournaments: 'Tournaments',
+      goodies: 'Goodies',
+      stream: 'Stream',
+    },
+    home: {
+      explain: {
+        title: '<p>What is <span style="color: rgb(102, 163, 224);">GameFit Week</span>?</p>',
+        description: '<p>Located in <span style="color: rgb(102, 163, 224);">Paris</span>, GameFit Week brings together enthusiasts, amateurs, and experts.</p>',
+      },
+      main: {
+        title: '<p>Welcome to <span style="color: rgb(102, 163, 224);">GameFit Week</span></p>',
+        description: '<p>The event that merges sports and esports into a unique experience.</p>',
+      },
+      cards: [
+        {
+          title: 'A Celebration of Sports and Esports',
+          description: 'Take part in thrilling competitions.',
+        },
+        {
+          title: 'Activities',
+          description: 'Join interactive activities.',
+        },
+        {
+          title: 'Services',
+          description: 'Explore high-quality services.',
+        },
+      ],
+      join_button: 'Join us',
+      carousel: {
+        slides: {
+          slide1: {
+            title: 'Signature Reservation',
+            text: 'Meet your esports and sports idols in person!<br>Book a private autograph session and leave with a personalized souvenir.',
+            link: 'See more',
+          },
+          slide2: {
+            title: 'Live Streaming',
+            text: 'Follow GameFit Week in real time with our high-definition streams.<br>Don’t miss any competition, interview, or key moment, wherever you are.',
+            link: 'See more',
+          },
+          slide3: {
+            title: 'Tournaments',
+            text: "Attend thrilling matches between sports and esports teams during GameFit Week.<br>Experience the intensity of the competition and support your favorite teams in spectacular showdowns.",
+            link: 'See more',
+          }
+        }
+
+    }
+    },
+  };
+  
---
+++ src/i18n/fr.js
@@ -0,0 +1,68 @@
+export default {
+    nav: {
+      home: 'Accueil',
+      ticketing: 'Billetterie',
+      services: 'Prestations',
+      live: 'En direct',
+      login: 'Se connecter',
+      logout: 'Déconnexion',
+      profile: 'Profil',
+      orders: 'Commandes',
+      reservations: 'Réservations',
+      tickets: 'Billets',
+      dashboard: 'Tableau de bord',
+      request: 'Demande',
+      providerList: 'Liste des prestataires',
+      dedicaces: 'Dédicaces',
+      tournaments: 'Tournois',
+      goodies: 'Goodies',
+      stream: 'En direct',
+    },
+    home: {
+        
+      explain: {
+        title: '<p>Qu’est-ce que <span style="color: rgb(102, 163, 224);">GameFit Week</span> ?</p>',
+        description: '<p>Situé à Paris, GameFit Week rassemble passionnés, amateurs et experts.</p>',
+      },
+      main: {
+        title: '<p>Bienvenue à <span style="color: rgb(102, 163, 224);">GameFit Week</span></p>',
+        description: '<p>L’événement qui fusionne sport et esport dans une expérience unique.</p>',
+      },
+      cards: [
+        {
+          title: 'Une célébration du sport et de l’esport',
+          description: 'Participez à des compétitions exaltantes.',
+        },
+        {
+          title: 'Les Activités',
+          description: 'Participez à des activités interactives.',
+        },
+        {
+          title: 'Les Services',
+          description: 'Explorez des services de qualité.',
+        },
+      ],
+      join_button: 'Rejoignez-nous',
+
+      carousel: {
+        slides: {
+          slide1: {
+            title: 'Réservation de Dédicaces',
+            text: 'Rencontrez vos idoles de l’esport et du sport en personne !<br>Réservez une séance de dédicaces privée et repartez avec un souvenir personnalisé.',
+            link: 'Voir plus',
+          },
+          slide2: {
+            title: 'Diffusion en direct',
+            text: 'Suivez GameFit Week en temps réel avec nos streams en haute définition.<br>Ne manquez aucune compétition, interview ou moment clé, où que vous soyez.',
+            link: 'Voir plus',
+          },
+          slide3: {
+            title: 'Les Tournois',
+            text: "Assistez aux matchs palpitants entre équipes sportives ou esportives lors de GameFit Week.<br>Suivez l'intensité de la compétition et soutenez vos équipes favorites dans des affrontements spectaculaires.",
+            link: 'Voir plus',
+          }
+        }
+    }
+    },
+  };
+  
