a51cc9530ac8a7a061e4472737e78a9479f9b105
BRENDLE Corentin <64205741+BrendleCorentin@users.noreply.github.com>
2025-04-02 19:50:32 +0200

    Update fr.js

--- src/i18n/fr.js
+++ src/i18n/fr.js
@@ -44,6 +44,7 @@ export default {
       ],
       join_button: 'Rejoignez-nous',
 
+
       carousel: {
         slides: {
           slide1: {
@@ -64,5 +65,100 @@ export default {
         }
     }
     },
+
+    login: {
+      title: "Connexion",
+      username: "Nom d'utilisateur",
+      username_placeholder: "Entrez votre nom d'utilisateur",
+      password: "Mot de passe",
+      password_placeholder: "Entrez votre mot de passe",
+      login_button: "Se connecter",
+      register_button: "Créer un compte",
+      provider_button: "Devenir Prestataire",
+      error_message: "Une erreur est survenue lors de la connexion.",
+    },
+
+    register: {
+      title: "Enregistrement",
+      email: "Email",
+      email_placeholder: "Entrez votre email",
+      name: "Nom Prénom",
+      name_placeholder: "Entrez votre nom et prénom",
+      username: "Nom d'utilisateur",
+      username_placeholder: "Entrez votre nom d'utilisateur",
+      password: "Mot de passe",
+      password_placeholder: "Entrez votre mot de passe",
+      register_button: "S'enregistrer",
+      cancel_button: "Annuler",
+      error_message: "Une erreur est survenue lors de l'enregistrement.",
+    },
+    reservation: {
+      number: "Numéro",
+      host: "Animateur",
+      stand: "Stand",
+      service: "Service",
+      date: "Date",
+      time: "Horaire",
+      operation: "Opération",
+      stand_label: "Stand",
+      cancel: "Annuler",
+    },
+    dedication: {
+      welcome_title: "Bienvenue dans le système de dédicace",
+      welcome_message: "Réservez un créneau pour rencontrer votre animateur préféré et obtenir une dédicace personnalisée !",
+      enter_ticket: "Entrez votre numéro de billet",
+      book_now: "Réservez maintenant",
+      easy_to_use: "Simple à utiliser",
+      easy_to_use_desc: "Une interface intuitive pour choisir votre animateur et réserver un créneau horaire rapidement.",
+      available_anytime: "Disponible à tout moment",
+      available_anytime_desc: "Réservez votre créneau horaire 24h/24, 7j/7 selon les disponibilités des animateurs.",
+      personalized_dedication: "Dédicaces personnalisées",
+      personalized_dedication_desc: "Obtenez une dédicace unique et mémorable pour vous ou vos proches.",
+    },
+    ticket: {
+      number: "Numéro",
+      date: "Date",
+      email: "Adresse e-mail",
+      age_category: "Catégorie d'âge",
+      price: "Prix",
+      actions: "Actions",
+      delete: "Supprimer",
+    },
+
+    provider_request: {
+      title: "Demande pour devenir prestataire",
+      number: "Numéro",
+      date: "Date",
+      services: "Services",
+      status: "Statut",
+      pending: "En attente",
+      validated: "Validé",
+      refused: "Refusé",
+      unknown: "Inconnu",
+      no_requests: "Aucune demande trouvée pour l'utilisateur actuel.",
+      no_services: "Aucun service trouvé pour l'utilisateur actuel.",
+      error_fetching: "Erreur lors de la récupération des données :",
+    },
+
+    order: {
+      title: "Vue des commandes",
+      no_order: "Aucune commande",
+      number: "Numéro de commande",
+      shop: "Boutique",
+      date: "Date",
+      ticket_number: "Numéro de ticket",
+      status: "État",
+      actions: "Actions",
+      show_details: "Voir le détail",
+      show_qr: "Afficher QR Code",
+      details_title: "Détails de la commande",
+      total: "Total",
+      in_preparation: "En préparation",
+      to_collect: "À venir récupérer",
+      collected: "Récupérée",
+      unknown: "Inconnu",
+    }
+
+    
   };
   
